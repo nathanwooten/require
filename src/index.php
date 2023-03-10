7156a9ac37aa7d20ad895fe2ae1d94eb9cdb43c2
Nathan Wooten <a8gid24akL@gmail.com>
2023-03-09 23:36:49 -0500

    require returns ProjectHandler

--- src/index.php
+++ src/index.php
@@ -1,5 +1,7 @@
 <?php
 
-require dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'require.php';
+$pj = require dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'require.php';
 
 // now that your dependencies are loaded, the application follows...
+
+$container = $pf->getResult( LOCAL . 'top.php' );
